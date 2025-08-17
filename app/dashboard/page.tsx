7164369ef766774ad29aa63e801885df541b4234
Emir Cokrlija <144956911+emirCo95@users.noreply.github.com>
2025-08-17 19:40:34 -0400

    minor fix

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,9 @@
+import Navbar from '@/components/navbar';
+
 export default function DashboardPage() {
-  return <div className="min-h-screen bg-gray-50"></div>;
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <Navbar />
+    </div>
+  );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 
 import { ClerkProvider } from '@clerk/nextjs';
-import Navbar from '@/components/navbar';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -31,7 +30,6 @@ export default function RootLayout({
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
-          <Navbar />
           {children}
         </body>
       </html>
