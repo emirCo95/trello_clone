adc081ad90de4bea94ea15efc02fa894672f175f
Emir Cokrlija <144956911+emirCo95@users.noreply.github.com>
2025-08-17 15:16:26 -0400

    Create navbar.tsx

---
+++ components/navbar.tsx
@@ -0,0 +1,16 @@
+'use client';
+
+import { Trello } from 'lucide-react';
+
+export default function Navbar() {
+  return (
+    <header>
+      <div className="">
+        <div className="">
+          <Trello />
+          <span>Trello Clone</span>
+        </div>
+      </div>
+    </header>
+  );
+}
